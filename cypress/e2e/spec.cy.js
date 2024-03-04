2e44febdc670c347254d8b2365bedb0c02c5b1db
Matt Munro <mrfmunro@gmail.com>
2024-03-04 21:36:07 +0000

    Add axe-core and cypress-axe for accessibility testing. Update test spec to run checkA11y function.

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,7 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('http://127.0.0.1:8080/')
+    cy.visit('http://127.0.0.1:8080/');
+    cy.injectAxe();
+    cy.checkA11y();
   })
 })
