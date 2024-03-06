c3621d1809076b3f7414d1f827ebb026280dfe2b
Matt Munro <mrfmunro@gmail.com>
2024-03-06 19:35:18 +0000

    Update test case descriptions.

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,5 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Basic accessibility checks', () => {
+  it('Checks Axe violations', () => {
     cy.visit('http://127.0.0.1:8080/');
     cy.injectAxe();
     cy.checkA11y();
